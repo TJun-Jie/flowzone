3cf726f08c38402b3c08703caaa6d72da75257ac
Jun Jie <62635032+TJun-Jie@users.noreply.github.com>
2024-02-17 17:31:21 -0800

    add sample data for action items and distractions

--- frontend/convex/schema.ts
+++ frontend/convex/schema.ts
@@ -2,15 +2,6 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  messages: defineTable({
-    body: v.string(),
-    user: v.id("users"),
-  }),
-  users: defineTable({
-    name: v.string(),
-    tokenIdentifier: v.string(),
-  }).index("by_token", ["tokenIdentifier"]),
-
   distractions: defineTable({
     name: v.string(),
     dateTime: v.number(),
